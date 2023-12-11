05e923de4ff53b2ae791deb3fb520ccf2a39a514
DivinPrince <dpcode36@gmail.com>
2023-12-11 17:56:19 +0200

    update product-form.tsx

--- app/(dashboard)/[storeId]/(routes)/(admin)/expences/[expenceId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/(admin)/expences/[expenceId]/components/product-form.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
-import * as z from "zod"
-import axios from "axios"
-import { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { toast } from "react-hot-toast"
-import { Trash } from "lucide-react"
-import { Expence, Product} from "@prisma/client"
-import { useParams, useRouter } from "next/navigation"
+import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { Trash } from "lucide-react";
+import { Expence, Product } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -20,58 +20,67 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Separator } from "@/components/ui/separator"
-import { Heading } from "@/components/ui/heading"
-import { AlertModal } from "@/components/modals/alert-modal"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import ImageUpload from "@/components/ui/image-upload"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Textarea } from "@/components/ui/textarea"
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { AlertModal } from "@/components/modals/alert-modal";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import ImageUpload from "@/components/ui/image-upload";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Textarea } from "@/components/ui/textarea";
 
 const formSchema = z.object({
   description: z.string().min(1),
   money: z.coerce.number().min(1),
 });
 
-type ProductFormValues = z.infer<typeof formSchema>
+type ProductFormValues = z.infer<typeof formSchema>;
 
 interface ProductFormProps {
   initialData: Expence | null;
-};
+}
 
-export const ProductForm: React.FC<ProductFormProps> = ({
-  initialData,
-}) => {
+export const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const title = initialData ? 'Edit product' : 'Create product';
-  const description = initialData ? 'Edit a product.' : 'Add a new product';
-  const toastMessage = initialData ? 'Product updated.' : 'Product created.';
-  const action = initialData ? 'Save changes' : 'Create';
+  const title = initialData ? "Edit expence" : "Create expence";
+  const description = initialData ? "Edit a expence." : "Add a new expence";
+  const toastMessage = initialData ? "expence updated." : "expence created.";
+  const action = initialData ? "Save changes" : "Create";
 
-  const defaultValues = initialData ? {
-    ...initialData,
-    money: parseFloat(String(initialData?.money)),
-  } : {
-    description: '',
-    money: 0,
-  }
+  const defaultValues = initialData
+    ? {
+        ...initialData,
+        money: parseFloat(String(initialData?.money)),
+      }
+    : {
+        description: "",
+        money: 0,
+      };
 
   const form = useForm<ProductFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues
+    defaultValues,
   });
 
   const onSubmit = async (data: ProductFormValues) => {
     try {
       setLoading(true);
       if (initialData) {
-        await axios.patch(`/api/${params.storeId}/expences/${params.expenceId}`, data);
+        await axios.patch(
+          `/api/${params.storeId}/expences/${params.expenceId}`,
+          data
+        );
       } else {
         await axios.post(`/api/${params.storeId}/expences`, data);
       }
@@ -79,7 +88,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
       router.push(`/${params.storeId}/expences`);
       toast.success(toastMessage);
     } catch (error: any) {
-      toast.error('Something went wrong.');
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
@@ -91,24 +100,24 @@ export const ProductForm: React.FC<ProductFormProps> = ({
       await axios.delete(`/api/${params.storeId}/expences/${params.expenceId}`);
       router.refresh();
       router.push(`/${params.storeId}/expences`);
-      toast.success('expe deleted.');
+      toast.success("expe deleted.");
     } catch (error: any) {
-      toast.error('Something went wrong.');
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
       setOpen(false);
     }
-  }
+  };
 
   return (
     <>
-    <AlertModal 
-      isOpen={open} 
-      onClose={() => setOpen(false)}
-      onConfirm={onDelete}
-      loading={loading}
-    />
-     <div className="flex items-center justify-between">
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
         {initialData && (
           <Button
@@ -123,16 +132,24 @@ export const ProductForm: React.FC<ProductFormProps> = ({
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
           <div className="md:grid md:grid-cols-3 gap-8">
             <FormField
               control={form.control}
-              name="description"
+              name="money"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Description</FormLabel>
+                  <FormLabel>Money</FormLabel>
                   <FormControl>
-                    <Textarea disabled={loading} placeholder="Color: white" {...field} />
+                    <Input
+                      type="number"
+                      disabled={loading}
+                      placeholder="100"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -140,12 +157,16 @@ export const ProductForm: React.FC<ProductFormProps> = ({
             />
             <FormField
               control={form.control}
-              name="money"
+              name="description"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>stockQuantity</FormLabel>
+                  <FormLabel>Description</FormLabel>
                   <FormControl>
-                    <Input type="number" disabled={loading} placeholder="100" {...field} />
+                    <Textarea
+                      disabled={loading}
+                      placeholder="Color: white"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>

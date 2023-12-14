6162952bcc5ca58e4f124e5482f76bb23060b6b5
DivinPrince <dpcode36@gmail.com>
2023-12-14 14:17:08 +0200

    update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = async () => {
     <>
       <div className="border-b hidden md:flex w-full">
         <div className="flex h-16 items-center px-4">
-          <StoreSwitcher items={stores} className="" />
+          <StoreSwitcher items={stores} />
           <MainNav className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
             <ThemeToggle />

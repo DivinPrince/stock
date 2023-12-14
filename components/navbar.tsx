cd626d15de6c99f6f52b0bd72287d9102520368c
DivinPrince <dpcode36@gmail.com>
2023-12-14 14:49:55 +0200

    build(deps): update page.tsx, navbar.tsx, package-lock.json and package.json

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,25 +31,31 @@ const Navbar = async () => {
 
   return (
     <>
-      <div className="border-b hidden md:flex w-full">
-        <div className="flex h-16 items-center px-4">
-          <StoreSwitcher items={stores} />
-          <MainNav className="mx-6" />
-          <div className="ml-auto flex items-center space-x-4">
-            <ThemeToggle />
-            <UserButton afterSignOutUrl="/" />
+      <div className="border-b w-full">
+        <div className="hidden md:flex">
+          <div className="flex h-16 items-center px-4">
+            <StoreSwitcher items={stores} />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <ThemeToggle />
+              <UserButton afterSignOutUrl="/" />
+            </div>
           </div>
         </div>
+        <div className="flex md:hidden">
+          <Sheet key="left">
+            <SheetTrigger>
+              <Menu />
+            </SheetTrigger>
+            <SheetContent side='left'>
+              <div className="flex flex-col items-end gap-2">
+                <StoreSwitcher items={stores} />
+                <MainNav className="mx-6 flex flex-col gap-2" />
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
-      <Sheet>
-        <SheetTrigger>
-          <Menu />
-        </SheetTrigger>
-        <SheetContent>
-          <StoreSwitcher items={stores} />
-          <MainNav className="mx-6 flex flex-col gap-2" />
-        </SheetContent>
-      </Sheet>
     </>
   );
 };

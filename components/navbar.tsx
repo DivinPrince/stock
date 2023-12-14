454c201f1ffc7172c2a8a30ca1d8261cbee9144e
DivinPrince <dpcode36@gmail.com>
2023-12-14 18:30:38 +0200

    update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -50,8 +50,8 @@ const Navbar = async () => {
             </SheetTrigger>
             <SheetContent side="left">
               <div className="flex flex-col items-start gap-2">
-                <SheetClose asChild>
                   <StoreSwitcher items={stores} />
+                <SheetClose asChild>
                   <MainNav className="flex flex-col gap-2 space-x-0 lg:space-x-0 items-start" />
                 </SheetClose>
               </div>

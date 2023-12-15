72cba5ebb797f3d3529f04c688b5666619e82498
DivinPrince <dpcode36@gmail.com>
2023-12-15 12:40:24 +0200

    update product-form.tsx

--- app/(dashboard)/[storeId]/(routes)/(admin)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/(admin)/products/[productId]/components/product-form.tsx
@@ -80,15 +80,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: ProductFormValues) => {
-    const already = await prismadb.product.findMany({
-      where: {
-        storeId: params.storeId as string,
-        name: data.name.toUpperCase(),
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    const already = await isAlready(params.storeId,data.name)
 
     if (already) {
       toast.error(

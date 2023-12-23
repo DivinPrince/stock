1f0d79c326e2c6d0db60782a799c176b8903fdeb
DivinPrince <dpcode36@gmail.com>
2023-12-23 10:55:35 +0200

    update cell-action.tsx

--- app/(dashboard)/[storeId]/(routes)/(admin)/report/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/(admin)/report/components/cell-action.tsx
@@ -79,8 +79,8 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   if (data.month == months[currentMonth]) {
     return (
       <>
-        <div className="bg-green-100 text-green-500 flex gap-2 w-fit p-1 rounded-full">
-          <CircleDot size={10} />
+        <div className="bg-green-100 text-green-500 flex gap-2 items-center w-fit p-1 rounded-full">
+          <CircleDot size={15} />
           <span>current</span>
         </div>
         <div ref={bottomRef} />

35efd98f4b7e6dbe10f6a6379e744796e3f4f896
DivinPrince <dpcode36@gmail.com>
2024-01-02 14:51:26 -0800

    ,ksh

--- app/(dashboard)/[storeId]/(routes)/seller/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/seller/page.tsx
@@ -33,9 +33,6 @@ const SellerPage = async ({ params }: { params: { storeId: string } }) => {
 
   return (
     <>
-    {userId !== "user_2ZFkhqgvmyN8kO9H7HyhS8FRYIN" && (
-      <Navbar />
-    )}
       <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <ProductsClient data={formattedProducts} />

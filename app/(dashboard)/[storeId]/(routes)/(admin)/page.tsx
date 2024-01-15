fc8d50ceeb5df2ec1fb2e59b69ece1d3036f6a4c
Divin Prince <124174236+DivinPrince@users.noreply.github.com>
2024-01-15 11:04:05 +0000

    purify

--- app/(dashboard)/[storeId]/(routes)/(admin)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/(admin)/page.tsx
@@ -151,7 +151,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
                 <CardHeader></CardHeader>
                 <CardContent className="pl-2">
                   <Table className="w-full">
-                    <TableCaption>{format(new Date(order.createdAt), "'Products Sold on' eeee") }</TableCaption>
+                    <TableCaption>{format(new Date(order.createdAt), "'Products Sold on' MM/dd/yyyy") }</TableCaption>
                     <TableHeader>
                       <TableRow className="flex justify-between">
                         <TableHead className="text-right">Time</TableHead>
@@ -182,7 +182,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
                     </div>
                   </Table>
                 </CardContent>
-              </Card>;
+              </Card>
           </>
         ))}
       </div>
